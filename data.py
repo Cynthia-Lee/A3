066e834ed951222ffbbbf81121609148624bdd0d
Cynthia Lee <cynth.lee23@gmail.com>
2021-04-05 04:40:32 -0400

    first commit

---
+++ data.py
@@ -0,0 +1,10 @@
+# Header
+# Consists of fields such as <From>, <Subject>, <Organization> and <Lines> fields.
+# The <lines> field includes the number of lines in the document body
+
+# Body
+# The main body of the document. This is where you should extract features from.
+
+# https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html?fbclid=IwAR2e_uoplSxSBWON3XZ69JA1Fnck-SFFE42PUKAVPi_quhe8CQk4qUnReWQ
+
+# load_files()
