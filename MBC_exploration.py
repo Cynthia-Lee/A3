75c1a92df5527cf1e1d9b8b8690c92f6f20fbe72
Cynthia Lee <cynth.lee23@gmail.com>
2021-04-10 05:17:35 -0400

    skipping feature selection

--- MBC_exploration.py
+++ MBC_exploration.py
@@ -7,7 +7,6 @@
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.metrics import precision_recall_fscore_support
-from sklearn.model_selection import GridSearchCV
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -15,15 +14,21 @@ from nltk.corpus import stopwords
 import re
 import string
 from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
 from nltk import word_tokenize
 #
 nltk.download('punkt')
+#
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import numpy as np
 from sklearn.linear_model import SGDClassifier
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import GenericUnivariateSelect, SelectPercentile, SelectKBest, SelectFpr, SelectFromModel, SelectFwe, SequentialFeatureSelector, RFE, RFECV, VarianceThreshold, chi2, f_classif, mutual_info_classif
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
 
 ### Dataset ###
 # Header
@@ -96,6 +101,10 @@ def stemming_tokenizer(text):
     stemmer = SnowballStemmer("english") # stemmer = SnowballStemmer("english", ignore_stopwords=True)
     return [stemmer.stem(w) for w in word_tokenize(text)]
 
+def lemma_tokenizer(text):
+    lemma = WordNetLemmatizer()
+    return [lemma.lemmatize(w) for w in word_tokenize(text)]
+
 # make sure that you preprocess your stop list to make sure that it is normalised like your tokens will be,
 # and pass the list of normalised words as stop_words to the vectoriser.
 
@@ -124,9 +133,17 @@ print("NB,C1," + result + "\n")
 #     print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
 
 # NB C1.5
-# print("Stopwords and Stemming")
+# print("Stopwords and Stemming or Lemma")
 # text_clf = Pipeline([
 #     ('vect', TfidfVectorizer(lowercase=True, tokenizer=stemming_tokenizer, stop_words=my_stop_words)), # vector
+#     ('clf', MultinomialNB()), # classifier
+# ])
+# text_clf.fit(twenty_train.data, twenty_train.target)
+# predicted = text_clf.predict(docs_test)
+# info = precision_recall_fscore_support(twenty_evaluation.target, predicted, average='macro')
+# result = ",".join(map(str,info[:-1]))
+# text_clf = Pipeline([
+#     ('vect', TfidfVectorizer(lowercase=True, tokenizer=lemma_tokenizer, stop_words=my_stop_words)), # vector
 #     ('clf', MultinomialNB()), # classifier
 # ])
 # text_clf.fit(twenty_train.data, twenty_train.target)
@@ -134,14 +151,6 @@ print("NB,C1," + result + "\n")
 # info = precision_recall_fscore_support(twenty_evaluation.target, predicted, average='macro')
 # result = ",".join(map(str,info[:-1]))
 # print("NB,C1.5," + result + "\n")
-# text_clf = Pipeline([
-#     ('vect', TfidfVectorizer(lowercase=True, tokenizer=stemming_tokenizer, stop_words=my_stop_words)), # vector
-#     ('clf', MultinomialNB()), # classifier
-# ])
-# text_clf.fit(twenty_train.data, twenty_train.target)
-# predicted = text_clf.predict(docs_test)
-# info = precision_recall_fscore_support(twenty_evaluation.target, predicted, average='macro')
-# result = ",".join(map(str,info[:-1]))
 # print("NB,C1.5," + result + "\n")
 
 ### Hyperparameters Tuning ##
@@ -160,7 +169,7 @@ print("NB,C2," + result + "\n")
 
 # NB C3
 text_clf = Pipeline([
-        ('vect', CountVectorizer(ngram_range=(1,4), lowercase=True, stop_words=my_stop_words)), # vector
+        ('vect', CountVectorizer(ngram_range=(1,2), lowercase=True, stop_words=my_stop_words)), # vector
         ('tfidf', TfidfTransformer(use_idf=True)), # transformer
         ('clf', MultinomialNB(alpha=0.001)), # classifier
     ])
@@ -179,8 +188,39 @@ print("NB,C3," + result + "\n")
 #     print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
 
 ### Feature Selection ###
+# For classification: chi2, f_classif, mutual_info_classif
 
+# NB C4
+estimator = DecisionTreeClassifier()
+c = chi2 # chi2, f_classif, mutual_info_classif
+try_features = [SelectPercentile(c), SelectKBest(c), SelectFpr(c), SelectFromModel(estimator), SelectFwe(c), VarianceThreshold()]
+# RFE(estimator), RFECV(estimator), SequentialFeatureSelector(estimator, cv=2)
+# lsvc = LinearSVC(C=0.01, penalty="l1", dual=False)
+# model = SelectFromModel(lsvc)
+# try_features = [model]
 
+for feature in try_features:
+    print(feature)
+    text_clf = Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1,2), lowercase=True, stop_words=my_stop_words)), # vector
+            ('tfidf', TfidfTransformer(use_idf=True)), # transformer
+            ('select', feature), # feature selection
+            ('clf', MultinomialNB(alpha=0.001)), # classifier
+        ])
+    text_clf.fit(twenty_train.data, twenty_train.target)
+    predicted = text_clf.predict(docs_test)
+    info = precision_recall_fscore_support(twenty_evaluation.target, predicted, average='macro')
+    result = ",".join(map(str,info[:-1]))
+    print("NB,C4," + result + "\n")
+
+# parameters = {
+#     #'vect__ngram_range': [(1, 1), (1, 2), (2, 2), (1, 3), (3, 3), (1, 4), (4, 4)],
+#     'select__percentile': (0, 100)
+# }
+# gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
+# gs_clf = gs_clf.fit(twenty_train.data, twenty_train.target)
+# for param_name in sorted(parameters.keys()):
+#     print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
 
 
 
@@ -191,9 +231,6 @@ print("NB,C3," + result + "\n")
 # L1, L2 or Lasso regularizers
 
 # https://scikit-learn.org/stable/modules/feature_selection.html
-
-# make sure that you preprocess your stop list to make sure that it is normalised like your tokens will be,
-# and pass the list of normalised words as stop_words to the vectoriser.
 
 class StemTokenizer:
     def __init__(self):
